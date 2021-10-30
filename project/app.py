18a099039ffeab4f8ed53bbaccdeb456a0e2e738
Jiaxing Leo Li <leojiaxing.li@mail.utoronto.ca>
2021-10-30 16:31:52 -0400

    Postgres Heroku: connect to remote db

--- project/app.py
+++ project/app.py
@@ -1,6 +1,6 @@
 import sqlite3
 from pathlib import Path
-
+import os
 from flask import Flask, g, render_template, request, session, \
                   flash, redirect, url_for, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
@@ -13,7 +13,10 @@ DATABASE = "flaskr.db"
 USERNAME = "admin"
 PASSWORD = "admin"
 SECRET_KEY = "change_me"
-SQLALCHEMY_DATABASE_URI = f'sqlite:///{Path(basedir).joinpath(DATABASE)}'
+SQLALCHEMY_DATABASE_URI = os.getenv(
+    'DATABASE_URL',
+    f'sqlite:///{Path(basedir).joinpath(DATABASE)}'
+)
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
